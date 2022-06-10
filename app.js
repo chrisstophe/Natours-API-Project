a8d8adbbb2fd452b4d51733e6e5ea8e159df2ba4
chrisstophe <christophe.rezk@gmail.com>
2022-06-10 18:45:19 -0400

    Added handling for POST requests

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Setting up a simple server with Express
 const app = express();
 
+// We need middleware, more details later
+app.use(express.json());
+
 // Quick Express Demo
 // Defining routes in Express
 // Specify the HTTP method and the URL
@@ -35,6 +38,31 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// Handling POST requests
+app.post('/api/v1/tours', (req, res) => {
+  //   "Creating" and ID for the new tour
+  const newId = tours[tours.length - 1].id + 1;
+  // Adding the ID to the tour
+  const newTour = Object.assign({ id: newId }, req.body);
+  // Add the new tour to the tours array
+  tours.push(newTour);
+  // Persist the new tours away to our JSON file = "Database"
+  // In a callback so make sure this is done ASYNC
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // Status 201 means created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
