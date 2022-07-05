17ea8a6a8fcf47866212fb0c512290bec5a4a4c0
chrisstophe <christophe.rezk@gmail.com>
2022-07-05 10:49:14 -0400

    Connected to mongoDB, defined a simple tour schema, model, and document

--- server.js
+++ server.js
@@ -1,7 +1,60 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+// Connecting mongoose to MongoDB
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  });
+
+// Creating a Tour Schema
+const tourScehma = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Creating a Tour Model
+const Tour = mongoose.model('Tour', tourScehma);
+
+// Creating a document out of the model
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+// Saving the tour document to MongoDB
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`ERROR !!!! ${err}`);
+  });
 
 ///////////////////////////////////////////////////////////////////////////////
 // Start Server
