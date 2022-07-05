f342b032aec6c5af6c216e997972806ae08be2c8
chrisstophe <christophe.rezk@gmail.com>
2022-07-05 12:52:40 -0400

    Updated the updateTour and deleteTour controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,19 +66,41 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // Not actually implementing updating the tour
-  res.status(200).json({
-    status: 'success',
-    // data: { tour: 'Updated tour here' },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const tour = await Tour.findByIdAndUpdate(id, body, {
+      // Setting new: true returns the modified document rather than the original
+      new: true,
+      ranValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // Not actually implementing deleting the tour
-  // Status 204 means no content
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Tour.findByIdAndDelete(id);
+    // Status 204 means no content
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
