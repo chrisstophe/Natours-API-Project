f20afa55cf9adb8da76736052b1642103789e27d
chrisstophe <christophe.rezk@gmail.com>
2022-07-06 17:21:37 -0400

    Added query middleware to hide secret tours and to calculate how long queries take

--- models/tourModel.js
+++ models/tourModel.js
@@ -52,6 +52,10 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -64,7 +68,7 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// Document Middleware runs before .save() and .create() but not insertMany()
+// Document Middleware that runs before .save() and .create() but not insertMany()
 tourSchema.pre('save', function (next) {
   // The this keyword points to the document currently being processed (saved)
   // Adding a slug field by slugifying the document name
@@ -83,6 +87,23 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// Query Middleware that runs before .find() to hide "secret" tours from results
+tourSchema.pre(/^find/, function (next) {
+  // Here, this refers to the query object
+  this.find({ secretTour: { $ne: true } });
+  // You can also set a property on the query object
+  this.start = Date.now();
+  next();
+});
+
+// Query Middleware that runs after .find() queries to calculate how long they took
+tourSchema.post(/^find/, function (docs, next) {
+  // Here, we have access to documents that were returned by the query
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 
 // Creating a Tour Model
 const Tour = mongoose.model('Tour', tourSchema);
