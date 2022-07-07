2d2726650826d5646ffb3bddf8775e969c68ecb9
chrisstophe <christophe.rezk@gmail.com>
2022-07-07 12:58:36 -0400

    Added the ndb debugger package and middleware to handle unhandled routes/requests

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('HELLO FROM THE MIDDLEWARE');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -33,5 +28,15 @@ app.use((req, res, next) => {
 // Mounted routers (also Middleware)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling unhandled routes
+// If we reach this point, that means the other middleware didn't handle the request
+// Always add these handlers at the end of the middleware stack
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
