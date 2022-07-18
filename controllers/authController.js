cf38eb71b55d618e208ecba8ca0e412c9fd363e1
chrisstophe <christophe.rezk@gmail.com>
2022-07-18 16:27:40 -0400

    Added signing users up with a JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,26 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
   // Create new user
-  const newUser = await User.create(req.body);
+  // If we just do User.create(req.body), anyone can signup as an Admin
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // Create the JWT
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    // Send the JWT to the client
+    token,
     data: {
       user: newUser,
     },
