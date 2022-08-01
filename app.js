64585c71e190375ae9dc39d9f14dbefd48d68c8a
chrisstophe <christophe.rezk@gmail.com>
2022-08-01 17:07:37 -0400

    Added data sanitization against NOSQL query injections and cross-site scripting attacks

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { fail } = require('assert');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +36,12 @@ app.use('/api', limiter);
 
 // Body parser: reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (cross-site scripting) attacks
+app.use(xss());
 
 // Middleware to serve static files
 app.use(express.static(`${__dirname}/public`));
